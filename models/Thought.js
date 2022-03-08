80063009d43a0759994622d167a652dd1ee04f37
DrDano <danielharned@gmail.com>
2022-03-08 14:33:30 -0800

    adding Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,55 +1,44 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema(
-    {
-      userName: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        validate: {
-            validator: e => {
-                const emailRegex = RegExp(
-                    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                  );
-                return e === emailRegex;
-            },
-            message: value => `${value} is not a valid email address.`
-        }
-      },
-      thoughts: [
-          {
-              type: Schema.Types.ObjectId,
-              ref: "Thought"
-          }
-      ],
-      friends: [
-          {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-          }
-      ],
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-      },
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 280,
     },
-    {
-      toJSON: {
-        virtuals: true,
-        getters: true
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
       },
-      id: false,
-    }
-  );
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.reduce((total, friends) => total + friends.length)
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.reduce((total, reactions) => total + reactions.length, 0);
 });
+
+const Thought = model("Pizza", ThoughtSchema);
+
+module.exports = Thought;
